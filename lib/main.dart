38cdb5995eeffa96571b1c8a33c2482abd9bb96a
furqan <furqankhanku@gmail.com>
2023-02-09 14:37:41 +0500

    testing project

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+    );
+  }
+
+ 
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My AppBar'),
+      ),
+      body: ,
+    );
+  }
+
+}
